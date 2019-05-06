218488c4187b6f763731b3b7a839d181df5c420d
郭宸玮 <windpurchasesnow@guochenweideMacBook-Pro.local>
2019-05-06 22:09:18 +0800

    createComponentDemo

--- src/components/test.vue
+++ src/components/test.vue
@@ -11,79 +11,89 @@
         <el-menu-item index="1" id="drinkPage" @click="getData('drink')">饮品</el-menu-item>
         <el-menu-item index="2" id="pastryPage" @click="getData('pastry')">糕点</el-menu-item>
         <el-menu-item id="mealPage" index="3" @click="getData('meal')">套餐</el-menu-item>
-        <el-menu-item index="4">已点商品</el-menu-item>
+        <el-menu-item index="4" ref="btn">已点商品</el-menu-item>
       </el-menu>
     </div>
     <div id="showZone">
       <div v-html="products"></div>
-      <div ref = 'show'  class="products"></div>
-      <div class="card">
-        <img
-          class="card-img-top vue2-preview"
-          height="150px"
-          width="150px"
-          src="../assets/moka.jpg"
-          alt
-        >
-        <div class="card-body">
-          <h4 class="card-title">摩卡</h4>
-          <p class="card-text">28</p>
-        </div>
+      <div ref="show" class="products">
+        <ul class="card-list">
+          <li v-for="(item, i) in cardList" :key="i">
+            <card-list :title="item.key" :price="item.value" src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"/>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import CardList from "@/components/CardList.vue";
 export default {
   name: "home",
+  components: {
+    CardList
+  },
   data() {
     return {
-      products: "<h1>good</h1>"
+      products: "<h1>good</h1>",
+      cardList: []
     };
   },
   methods: {
     getData: function(e) {
-      this.$refs.show.innerHTML = '';
+      let that = this;
       console.log(e);
       if (e == null || e == "") {
         e = "default";
       }
-      var json = [
+      let json = [
         { moka: 28, Latte: 26, Capucino: 123 },
         { Tilamisu: 20, Cheese: 22 }
       ];
 
-      for (var i = 0, l = json.length; i < l; i++) {
-        for (var key in json[i]) {
+      for (let i = 0, l = json.length; i < l; i++) {
+        for (let key in json[i]) {
           console.log(key + ":" + json[i][key]);
-          this.CardCreate(key, json[i][key]);
+          that.cardList.push({
+            key,
+            value: json[i][key]
+          });
         }
       }
     },
-  CardCreate: function(key, value){
-    var path = '../assets/moka.jpg'
-    var newCard = 
-      '<div class="card">' +
-        '<img' + 
-          'class="card-img-top"' +
-          'height="150px"' +
-          'width="150px"' + 
-          'src='+ path +' ' 
-          'alt' +
-        '>' + 
-        '<div class="card-body">' +
-          '<h4 class="card-title">' + key + '</h4>' +
-          '<p class="card-text">' + value + '</p>'
-
-      this.$refs.show.innerHTML += newCard;
+    clickEvent () {
+      
+    }
   },
   mounted() {
     this.getData();
   }
-  },
 };
 </script>
 
 <style scoped>
+.products {
+  width: 940px;
+  margin: 0 auto;
+  overflow: hidden;
+}
+.card-list {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  text-align: left;
+  margin-right: -20px;
+}
+.card-list li {
+  display: inline-block;
+  width: 300px;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
+.card-list::after {
+  content: "";
+  clear: both;
+  display: block;
+}
 </style>
